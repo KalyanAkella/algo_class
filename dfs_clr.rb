7f261fb6b9ed87fe55eb77553778ab602d302993
Kalyan Akella <kalyan.akella@gmail.com>
2012-07-09 22:00:11 +0530

    All edges are reported with their type. All nodes are updated with their connected component number

--- dfs_clr.rb
+++ dfs_clr.rb
@@ -1,11 +1,11 @@
 class Node
-  attr_accessor :d, :f, :id, :color, :others
+  attr_accessor :d, :f, :id, :color, :others, :cc
 
   def initialize(id)
     @id = id
     @others = []
     @color = 'W'
-    @d = @f = -1
+    @d = @f = @cc = -1
   end
 
   def to(node)
@@ -26,9 +26,34 @@ end
 
 def dfs(nodes)
   time = 0
+  cc = 0
   nodes.each do |node|
-    time = dfs_visit(node, time) if node.white?
+    if node.white?
+      time = dfs_visit_recurse(node, time, cc)
+      cc += 1
+    end
   end
+end
+
+# recursive
+def dfs_visit_recurse(node, time, cc)
+  node.color = 'G'
+  time += 1
+  node.d = time
+  node.each_other_node do |other_node|
+    if other_node.white?
+      puts "#{node.id}~>#{other_node.id}=>Tree Edge"
+      time = dfs_visit_recurse(other_node, time, cc)
+    else
+      puts "#{node.id}~>#{other_node.id}=>Backward Edge" if other_node.color == 'G'
+      puts "#{node.id}~>#{other_node.id}=>Cross/Forward Edge" if other_node.color == 'B'
+    end
+  end
+  node.color = 'B'
+  time += 1
+  node.f = time
+  node.cc = cc
+  time
 end
 
 def dfs_visit(node, time)
@@ -53,21 +78,6 @@ def dfs_visit(node, time)
       end
     end
   end
-  time
-end
-
-def dfs_visit1(node, time)
-  node.color = 'G'
-  time += 1
-  node.d = time
-  node.each_other_node do |other_node|
-    if other_node.white?
-      time = dfs_visit1(other_node, time)
-    end
-  end
-  node.color = 'B'
-  time += 1
-  node.f = time
   time
 end
 
@@ -97,5 +107,5 @@ graph = [q,r,s,t,u,v,w,x,y,z]
 dfs(graph)
 
 graph.each do |_node|
-  puts "#{_node.id} -> #{_node.d}/#{_node.f}"
+  puts "#{_node.id} -> (#{_node.d}/#{_node.f}),#{_node.cc}"
 end
